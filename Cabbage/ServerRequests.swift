34ef41d6ef516765834d7eebd67fcc679685d925
ChrisCRW <ChrisRoberts-Watts@Chriss-MacBook-Pro-3.local>
2017-02-21 15:19:38 +0000

    - Sorted out first page load up and added blank server request class

---
+++ Cabbage/ServerRequests.swift
@@ -0,0 +1,10 @@
+//
+//  ServerRequests.swift
+//  Cabbage
+//
+//  Created by Chris Roberts-Watts on 21/02/2017.
+//  Copyright © 2017 Self-Ignition. All rights reserved.
+//
+
+import Foundation
+
