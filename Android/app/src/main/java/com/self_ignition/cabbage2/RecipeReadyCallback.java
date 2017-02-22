09565468e10e25e82b77d63be525059f9308739a
ThatSlothGuy <c.trueman@hotmail.co.uk>
2017-02-22 23:56:34 +0000

    suicide watch 2k17

--- Android/app/src/main/java/com/self_ignition/cabbage2/RecipeReadyCallback.java
+++
@@ -1,5 +0,0 @@
-package com.self_ignition.cabbage2;
-
-public interface RecipeReadyCallback{
-    void onReady(Recipe r);
-}
--- Android/app/src/main/java/com/self_ignition/cabbage2/VolleyCallback.java
+++
@@ -1,9 +0,0 @@
-package com.self_ignition.cabbage2;
-
-/**
- * Created by r-bur on 10/02/2017.
- */
-
-public interface VolleyCallback {
-        void onSuccess(String result);
-}
