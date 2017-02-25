f4d4e4a7c5110f2a1009289b4cd3798fabe6b950
Bartlomiej Morawiec <ayylmao@Bartlomiejs-MacBook-Pro.local>
2017-02-25 12:15:16 +0100

    Added classes to My Fridge page.

---
+++ Cabbage/MyFridgeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyFridgeTableViewCell.swift
+//  Cabbage
+//
+//  Created by Bartlomiej Morawiec on 25/02/2017.
+//  Copyright © 2017 Self-Ignition. All rights reserved.
+//
+
+import UIKit
+
+class MyFridgeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
