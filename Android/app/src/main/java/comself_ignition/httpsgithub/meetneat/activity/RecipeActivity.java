05abdf1cc7c7d4776b043969c5c679915ed293f6
ThatSlothGuy <c.trueman@hotmail.co.uk>
2017-03-22 18:43:46 +0000

    Fab for reviews

--- Android/app/src/main/java/comself_ignition/httpsgithub/meetneat/activity/RecipeActivity.java
+++ Android/app/src/main/java/comself_ignition/httpsgithub/meetneat/activity/RecipeActivity.java
@@ -3,62 +3,27 @@ package comself_ignition.httpsgithub.meetneat.activity;
 import android.animation.ValueAnimator;
 import android.app.FragmentManager;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.BaseExpandableListAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
-import org.w3c.dom.Text;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import comself_ignition.httpsgithub.meetneat.R;
-import comself_ignition.httpsgithub.meetneat.other.CircleTransform;
-import comself_ignition.httpsgithub.meetneat.other.FriendAction;
 import comself_ignition.httpsgithub.meetneat.other.ModelObject;
 import comself_ignition.httpsgithub.meetneat.other.Recipe;
 import comself_ignition.httpsgithub.meetneat.other.RecipeReadyCallback;
+import comself_ignition.httpsgithub.meetneat.other.ReviewsDialogFragment;
 import comself_ignition.httpsgithub.meetneat.other.SaveSharedPreference;
 import comself_ignition.httpsgithub.meetneat.other.SavedRecipeAction;
 import comself_ignition.httpsgithub.meetneat.other.ServerRequests;
 import comself_ignition.httpsgithub.meetneat.other.VolleyCallback;
-
-import static comself_ignition.httpsgithub.meetneat.R.id.Reviews;
-import static comself_ignition.httpsgithub.meetneat.R.string.recipe;
 
 public class RecipeActivity extends AppCompatActivity implements VolleyCallback, RecipeReadyCallback {
     Recipe recipe = new Recipe();
@@ -186,7 +151,6 @@ public class RecipeActivity extends AppCompatActivity implements VolleyCallback,
         String reviews_text = recipe.getReviews().toString();
         reviews_text = reviews_text.replace("[","");
         reviews_text = reviews_text.replace("]","");
-        reviews_text = reviews_text.replace(".,", "\n\n");
         review.setText(reviews_text);
 
         Glide.with(this)
@@ -233,42 +197,5 @@ class CustomPagerAdapter extends PagerAdapter {
     public CharSequence getPageTitle(int position) {
         ModelObject customPagerEnum = ModelObject.values()[position];
         return mContext.getString(customPagerEnum.getTitleResId());
-    }
-}
-
-class ReviewsDialogFragment extends DialogFragment{
-
-    String id;
-
-    public ReviewsDialogFragment(String id) {
-        this.id = id;
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View rootView = inflater.inflate(R.layout.activity_recipe_review_dialog, container, false);
-        getDialog().setTitle("Simple Dialog");
-        final Button add = (Button) rootView.findViewById(R.id.submit);
-        add.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                EditText review = (EditText) rootView.findViewById(R.id.input_addReview);
-                String str = review.getText().toString();
-                ServerRequests ser = new ServerRequests();
-                ser.AddReview(getActivity(), SaveSharedPreference.getUserName(getActivity()), id, str);
-                dismiss();
-            }
-        });
-
-        Button dismiss = (Button) rootView.findViewById(R.id.cancel);
-        dismiss.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                dismiss();
-            }
-        });
-        return rootView;
     }
 }
