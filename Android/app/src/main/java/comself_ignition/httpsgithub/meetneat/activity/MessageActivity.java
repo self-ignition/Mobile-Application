6f784df90832c6bdf4bb63b22d7f628f520b6121
ThatSlothGuy <c.trueman@hotmail.co.uk>
2017-03-26 20:50:18 +0100

    message layouts

---
+++ Android/app/src/main/java/comself_ignition/httpsgithub/meetneat/activity/MessageActivity.java
@@ -0,0 +1,16 @@
+package comself_ignition.httpsgithub.meetneat.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comself_ignition.httpsgithub.meetneat.R;
+
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
--- Android/app/src/main/java/comself_ignition/httpsgithub/meetneat/fragment/MessagesFragment.java
+++ Android/app/src/main/java/comself_ignition/httpsgithub/meetneat/fragment/MessagesFragment.java
@@ -1,27 +1,110 @@
 package comself_ignition.httpsgithub.meetneat.fragment;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.MenuItemCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import comself_ignition.httpsgithub.meetneat.R;
+import comself_ignition.httpsgithub.meetneat.activity.SearchResultsActivity;
+import comself_ignition.httpsgithub.meetneat.other.FriendAction;
+import comself_ignition.httpsgithub.meetneat.other.MyFriendsDialogFragment;
+import comself_ignition.httpsgithub.meetneat.other.SaveSharedPreference;
+import comself_ignition.httpsgithub.meetneat.other.ServerRequests;
+import comself_ignition.httpsgithub.meetneat.other.VolleyCallback;
 
-public class MessagesFragment extends Fragment {
+import static android.R.id.list;
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+public class MessagesFragment extends Fragment implements VolleyCallback{
+
+    Map<String, Boolean> friends = new HashMap<>();
+    ListView list;
 
 
     public MessagesFragment() {
         // Required empty public constructor
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setHasOptionsMenu(true);
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater = getActivity().getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        MenuItem searchViewItem = menu.findItem(R.id.search);
+        final SearchView searchViewAndroidActionBar = (SearchView) MenuItemCompat.getActionView(searchViewItem);
+        searchViewAndroidActionBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchViewAndroidActionBar.clearFocus();
+                doMySearch(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    public void doMySearch(String query) {
+        Intent intent = new Intent(getActivity(), SearchResultsActivity.class);
+        intent.putExtra("query", query);
+        getActivity().startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.action_addFriend:
+                FragmentManager fm = getFragmentManager();
+                final MyFriendsDialogFragment dialogFragment = new MyFriendsDialogFragment();
+                dialogFragment.show(fm, "Sample Fragment");
+
+                fm.executePendingTransactions();
+                dialogFragment.getDialog().setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        onResume();
+                    }
+                });
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -31,4 +114,132 @@ public class MessagesFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_messages, container, false);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        list = (ListView) getActivity().findViewById(R.id.messageList);
+        Context c = getActivity().getApplicationContext();
+        ServerRequests sr = new ServerRequests();
+        sr.Friends(c, this, FriendAction.getSender, SaveSharedPreference.getUserName(c), "");
+    }
+
+    @Override
+    public void onSuccess(String result) {
+        String[] friends = result.split("¦");
+        try{
+            for (String friend : friends) {
+                String name = friend.split("\\|")[0];
+                Boolean confirmed;
+                if(friend.split("\\|")[1].charAt(0) == '1')
+                {
+                    confirmed = true;
+                }
+                else {
+                    confirmed = false;
+                }
+                this.friends.put(name, confirmed);
+            }
+            UpdateList();
+        } catch(IndexOutOfBoundsException e) {
+            Log.e("RESULT", result);
+        }
+
+    }
+
+    private void UpdateList() {
+        List<String> names = new ArrayList<>();
+        for (String s : friends.keySet()) {
+            names.add(s);
+        }
+        Collections.sort(names);
+        list=(ListView) getActivity().findViewById(R.id.messageList);
+        list.setAdapter(new adapterMessages(getActivity(),friends, names, this));
+    }
+}
+
+class adapterMessages extends ArrayAdapter<String> implements VolleyCallback {
+    Context context;
+    Map<String, Boolean> friends = new HashMap<>();
+    List<String> names;
+    VolleyCallback callback;
+
+    adapterMessages(Context c, Map<String, Boolean> friends, List<String> names, final VolleyCallback callback) {
+        super(c, R.layout.fragment_friends_row, names);
+        this.names = names;
+        this.context = c;
+        this.friends = friends;
+        this.callback = callback;
+    }
+
+    @Override
+    public int getCount() {
+        return names.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        if(friends.get(names.get(position))) {
+            //Not pending request, should have menu button in it
+            View row = inflater.inflate(R.layout.fragment_friends_row, parent, false);
+            TextView name = (TextView) row.findViewById(R.id.FriendName);
+
+            final String friendName = friends.keySet().toArray()[position].toString();
+
+            final ImageView menuButton = (ImageView) row.findViewById(R.id.friends_menu_button);
+            menuButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show();
+                    PopupMenu popup = new PopupMenu(context, menuButton);
+                    //Inflating the Popup using xml file
+                    popup.getMenuInflater().inflate(R.menu.friends_menu, popup.getMenu());
+
+                    //registering popup with OnMenuItemClickListener
+                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId())
+                            {
+                                case R.id.Message_Button:
+                                    Toast.makeText(context, "Suck me off", Toast.LENGTH_SHORT).show();
+                                    return true;
+                                case R.id.Remove_Button:
+                                    ServerRequests req = new ServerRequests();
+                                    req.Friends(context, callback, FriendAction.remove, SaveSharedPreference.getUserName(context), friendName);
+                                    return true;
+                                default:
+                                    return true;
+                            }
+                        }
+                    });
+
+                    popup.show();
+                }
+            });
+
+            name.setText(names.get(position).toString());
+
+            return row;
+        } else {
+            //Pending Friend request
+            View row = inflater.inflate(R.layout.fragment_friends_pending_row, parent, false);
+            TextView name = (TextView) row.findViewById(R.id.FriendName);
+
+            name.setText(names.get(position).toString());
+
+            return row;
+        }
+    }
+
+    @Override
+    public void onSuccess(String result) {
+        Log.i("FOCUS YOU FUCK", "onSuccess: "  + result);
+    }
 }
