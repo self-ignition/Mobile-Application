9861aeeb9a02158a0838999e590c536664813f93
Ross Burchnall <cryo.dor@gmail.com>
2017-03-27 16:29:30 +0100

    Message Adapter gubbins

--- Android/app/src/main/java/comself_ignition/httpsgithub/meetneat/activity/MessageActivity.java
+++ Android/app/src/main/java/comself_ignition/httpsgithub/meetneat/activity/MessageActivity.java
@@ -3,7 +3,12 @@ package comself_ignition.httpsgithub.meetneat.activity;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,5 +48,52 @@ public class MessageActivity extends AppCompatActivity implements VolleyCallback
     @Override
     public void onSuccess(String result) {
         conversation = new Conversation(result, Sender);
+    }
+}
+
+class adapterMessage extends ArrayAdapter<Message> {
+    Conversation conversation;
+    Context context;
+    String Sender;
+
+    adapterMessage(Context c, Conversation conversation) {
+        super(c, R.layout.activity_search_results_row, conversation.getAggreateConversation());
+        this.context = c;
+        this.conversation = conversation;
+        this.Sender = SaveSharedPreference.getUserName(c);
+    }
+
+    @Override
+    public int getCount() {
+        return conversation.Count();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (conversation.getAggreateConversation().get(position).getSender().equals(Sender)) {
+            //Not pending request, should have menu button in it
+            View row = inflater.inflate(R.layout.activity_message_outgoing_row, parent, false);
+            TextView name = (TextView) row.findViewById(R.id.incomingMessage);
+            name.setText(names.get(position).toString());
+
+
+            return row;
+        } else {
+            //Pending Friend request
+            View row = inflater.inflate(R.layout.activity_message_incoming_row, parent, false);
+            TextView name = (TextView) row.findViewById(R.id.outgoingMessage);
+
+            name.setText(names.get(position).toString());
+
+            return row;
+        }
+
     }
 }
