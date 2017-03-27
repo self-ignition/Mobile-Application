b9428353cd75cb414a6e8616e6f45543de6a6b61
ThatSlothGuy <c.trueman@hotmail.co.uk>
2017-03-27 18:35:28 +0100

    message layouts

--- Android/app/src/main/java/comself_ignition/httpsgithub/meetneat/fragment/SettingsFragment.java
+++ Android/app/src/main/java/comself_ignition/httpsgithub/meetneat/fragment/SettingsFragment.java
@@ -6,11 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import comself_ignition.httpsgithub.meetneat.R;
 import comself_ignition.httpsgithub.meetneat.other.ResetPassword;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
     public SettingsFragment() {
         // Required empty public constructor
     }
@@ -18,18 +19,23 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Button btn = (Button) rootView.findViewById(R.id.btn_resetPassword);
+        btn.setOnClickListener(this);
+        return rootView;
     }
 
-    public void resetPassword(View V){
-        Intent reset = new Intent(getContext(),ResetPassword.class);
-        getContext().startActivity(reset);
+    @Override
+    public void onClick(View v) {
+        Intent reset = new Intent(getActivity(),ResetPassword.class);
+        getActivity().startActivity(reset);
     }
-
 }
